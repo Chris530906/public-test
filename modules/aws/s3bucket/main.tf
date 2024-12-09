86d1167e5c7363652362c7ac9c0bcc0169d1518c
Chris Fox <chris.f@quali.com>
2024-12-09 15:53:43 -0800

    change

--- modules/aws/s3bucket/main.tf
+++ modules/aws/s3bucket/main.tf
@@ -7,6 +7,20 @@ resource "aws_s3_bucket" "bucket" {
  tags = {
     Name        = "My bucket"
     Environment = "Dev"
-    AMAZING_TAG = "exterior"
+    AMAZING_TAG = "changed"
   }
 }
+
+resource "aws_s3_bucket" "bucket22" {
+  bucket = "gdflngfdlkndgflkngflkgnflkdgnf"
+  force_destroy = true  
+
+
+
+ tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    AMAZING_TAG = "changed-2nd"
+  }
+}
+
