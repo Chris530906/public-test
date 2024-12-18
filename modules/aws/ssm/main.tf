46a90b6af3b71808fa3a69fd8fcccd6abf409d12
Chris Fox <chris.f@quali.com>
2024-12-18 09:09:07 -0800

    ssm data info

--- modules/aws/ssm/main.tf
+++ modules/aws/ssm/main.tf
@@ -14,6 +14,12 @@ terraform {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
 # variable "role_arn" {
 #   type = string
 # }
