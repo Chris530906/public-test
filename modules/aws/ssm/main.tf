ce7b3d49d3678a19b3ff1c3844d2cd198654b2a4
Chris Fox <chris.f@quali.com>
2024-12-20 12:37:30 -0800

    terragrunt

--- modules/aws/ssm/main.tf
+++ modules/aws/ssm/main.tf
@@ -1,19 +1,19 @@
-provider "aws" {
-  region  = var.aws_region
-  # profile = "test"
-  # assume_role {
-  #   role_arn    = var.role_arn
-  # }
-}
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = ">3.0.0"
-#     }
-#   }
+# provider "aws" {
+#   region  = var.aws_region
+#   # profile = "test"
+#   # assume_role {
+#   #   role_arn    = var.role_arn
+#   # }
 # }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">3.0.0"
+    }
+  }
+}
 
 data "aws_caller_identity" "current" {}
 
