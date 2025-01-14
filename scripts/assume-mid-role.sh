cb89d9da29850e9cc34d326cf41d298d9c66b747
Chris Fox <chris.f@quali.com>
2025-01-14 05:52:24 -0800

    if testing

--- scripts/assume-mid-role.sh
+++ scripts/assume-mid-role.sh
@@ -7,22 +7,26 @@ DEFAULT_REGION=$3
 
 aws sts get-caller-identity
 
-# if (ROLE_ARN -eq aws sts get-caller-identity --query Arn)
-
-current_arn=$(aws sts get-caller-identity --query Arn --output text)
-target_arn=$1
-
-current_role=$(echo "$current_arn" | sed -n 's|.*:assumed-role/\([^/]*\)/.*|\1|p')
-current_account=$(echo "$curren_arn" | sed -n 's|.*:sts::\([0-9]*\):.*|\1|p')
-
-# Extract the role name and account ID from the target ARN
-target_role=$(echo "$target_arn" | sed -n 's|.*:role/\([^/]*\)$|\1|p')
-target_account=$(echo "$target_arn" | sed -n 's|.*:iam::\([0-9]*\):.*|\1|p')
-
-if [[ "$current_role" == "$target_role" && "$current_account" == "$target_account" ]]; then
-  echo "Already in the target role ($current_role in account $current_account). Doing nothing."
-  exit 0
+if (MID_ROLE_SET); then
+  if (MID_ROLE_SET -eq '1'); then
+    echo "Already in the target role ($current_role in account $current_account). Doing nothing."
+    exit 0
+  fi
 fi
+# current_arn=$(aws sts get-caller-identity --query Arn --output text)
+# target_arn=$1
+
+# current_role=$(echo "$current_arn" | sed -n 's|.*:assumed-role/\([^/]*\)/.*|\1|p')
+# current_account=$(echo "$curren_arn" | sed -n 's|.*:sts::\([0-9]*\):.*|\1|p')
+
+# # Extract the role name and account ID from the target ARN
+# target_role=$(echo "$target_arn" | sed -n 's|.*:role/\([^/]*\)$|\1|p')
+# target_account=$(echo "$target_arn" | sed -n 's|.*:iam::\([0-9]*\):.*|\1|p')
+
+# if [[ "$current_role" == "$target_role" && "$current_account" == "$target_account" ]]; then
+#   echo "Already in the target role ($current_role in account $current_account). Doing nothing."
+#   exit 0
+# fi
 
 echo "temp file: $temp_file"
 
@@ -40,10 +44,12 @@ NEW_AWS_ROLE_ARN=$(aws sts get-caller-identity --query Arn)
 export AWS_ROLE_ARN=$NEW_AWS_ROLE_ARN
 export AWS_PROFILE=$PROFILE
 export AWS_WEB_IDENTITY_TOKEN_FILE=""
+export MID_ROLE_SET='1'
 
 echo "export AWS_PROFILE=$PROFILE" >> ~/torque-envs.sh
 echo "export AWS_ROLE_ARN=$NEW_AWS_ROLE_ARN" >> ~/torque-envs.sh
 echo 'export AWS_WEB_IDENTITY_TOKEN_FILE=""' >> ~/torque-envs.sh
+echo 'export MID_ROLE_SET='1'' >> ~/torque-envs.sh
 
 aws sts get-caller-identity
 
