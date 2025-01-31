b140c69d64c9fcf2ab3eba1a02a133884571afd7
Chris Fox <chris.f@quali.com>
2025-01-30 17:35:32 -0800

    label examples

---
+++ labels/scripts/labels/unpause.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+url = f"https://portal.qtorque.io/api/spaces/{sys.argv[1]}/environments/{sys.argv[2]}/labels"
+
+payload = {
+    "added_labels": [
+        {
+            "key": "status",
+            "value": "Active"
+        }
+    ],
+    "removed_labels": [
+        {
+            "key": "status",
+            "value": "Paused"
+        }
+    ]
+}
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {sys.argv[3]}"
+}
+
+response = requests.put(url, json=payload, headers=headers)
+
+if response.status_code != 200:
+    sys.exit(1)
